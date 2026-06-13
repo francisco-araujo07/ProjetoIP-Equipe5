fe1787f4192be872513ba7d839bafaf706d9e488
Francisco <fela@cin.ufpe.br>
2026-06-13 09:54:58 -0300

    update: Task finalizada

--- core/game.py
+++ core/game.py
@@ -1,2 +1,36 @@
+import pygame 
+import settings
+from core.level import Level
+
+
 class Game:
-    pass
+    def __init__(self):
+        pygame.init()
+        self.tela = pygame.display.set_mode((settings.LARGURA_TELA, settings.ALTURA_TELA))
+        pygame.display.set_caption(settings.TITULO_TESTE)
+        self.clock = pygame.time.Clock()
+        self.rodando = True
+        self.nivel = Level()
+
+
+    def processar_eventos(self):
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    self.rodando = False
+
+    def atualizar(self):
+        self.nivel.atualizar()
+    
+    def desenhar(self):
+        self.nivel.desenhar(self.tela)
+        pygame.display.flip()
+
+
+    def rodar(self):
+        while self.rodando:
+            self.processar_eventos()
+            self.atualizar()
+            self.desenhar()
+            self.clock.tick(settings.FPS)
+
+        pygame.quit()
--- core/level.py
+++ core/level.py
@@ -1,2 +1,14 @@
+import settings
+
 class Level:
-    pass
+    def __init__(self):
+        pass
+
+    def atualizar(self):
+        pass 
+
+    def desenhar(self, tela):
+        tela.fill(settings.BLUE)  # Limpa a tela com preto
+
+    def terminou(self):
+        return False 
